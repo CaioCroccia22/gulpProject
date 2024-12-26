3ed95541b3e8ec9ec1168319da952e357d20bdfc
caiocroccia22 <caiocroccia22@gmail.com>
2024-12-25 22:39:27 -0300

    feat: Execute taks in serial mode

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+//Importação do pacote
+const gulp = require('gulp')
 function firstFunction(callback){
     console.log('Executando via Gulp');
     callback();
@@ -8,9 +10,19 @@ function dizOi(callback){
     callback();
 }
 
-exports.default = firstFunction;
-exports.dizOi = dizOi;
+
+//Default é a tarefa padrão
+// exports.default = firstFunction;
+
+
+// Criação de tarefa dizOi
+// exports.dizOi = dizOi;
 
 //Tarefas publicas -> São aquelas que exportamos 
 //Tarefas privadas -> São aquelas que não exportamos, mas podemos utiliza-las dentro de outras
 //tarefas
+
+
+//Para exporta tarefas em serie
+
+exports.default = gulp.series(firstFunction, dizOi);
