1f695de8faeba63ead30276212e82890a59d7c7e
caiocroccia22 <caiocroccia22@gmail.com>
2024-12-26 21:27:52 -0300

    feat: execute tasks with Gulp in parallel mode

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 //Importação do pacote
 const gulp = require('gulp')
 function firstFunction(callback){
-    console.log('Executando via Gulp');
-    callback();
+    setTimeout(function(){
+        console.log('Executando via Gulp');
+        callback();
+    }, 10000)
 }
 
 function dizOi(callback){
@@ -25,4 +27,7 @@ function dizOi(callback){
 
 //Para exporta tarefas em serie
 
-exports.default = gulp.series(firstFunction, dizOi);
+// exports.default = gulp.series(firstFunction, dizOi);
+
+//Para exporta tarefas em paralelo
+exports.default = gulp.parallel(firstFunction, dizOi);
